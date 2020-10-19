a51614bf36dec0c5b0761b09c65734727bebaa08
HumbertoML <humberto_molinalopez@hotmail.com>
2020-10-19 00:23:25 -0500

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.ApplicationModel.DataTransfer;
+using System.Threading.Tasks;
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace ClipboardCSharpUWP
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        public void InputClipboardText(string text)
+        {
+            DataPackage dataPackage = new DataPackage
+            {
+                RequestedOperation = DataPackageOperation.Copy
+            };
+            dataPackage.SetText(text);
+            Clipboard.SetContent(dataPackage);
+        }
+        public static async Task<string> OutputClipboardText()
+        {
+            DataPackageView dataPackageView = Clipboard.GetContent();
+
+            if (dataPackageView.Contains(StandardDataFormats.Text))
+            {
+                string text = await dataPackageView.GetTextAsync();
+                return text;
+            }
+            return await Task.FromResult(String.Empty);
+        }
+        private void BtnCopiar_Click_1(object sender, RoutedEventArgs e)
+        {
+            InputClipboardText(TxtTexto.Text);
+        }
+
+        private async void BtnPegar_Click_1(object sender, RoutedEventArgs e)
+        {
+            TxtTexto.Text = await OutputClipboardText();
+        }
+    }
+}
